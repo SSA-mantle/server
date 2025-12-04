0036854ab108a641a65a6e0bf623354bf9032a50
road3144 <fnfnfn258@gmail.com>
2025-12-04 17:57:15 +0900

    init

---
+++ application/src/test/java/com/pigeon3/ssamantle/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigeon3.ssamantle.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
