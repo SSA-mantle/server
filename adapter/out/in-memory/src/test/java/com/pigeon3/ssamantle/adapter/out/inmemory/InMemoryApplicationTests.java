c0f623622166692e153024c15249b50664b0b136
road3144 <fnfnfn258@gmail.com>
2025-12-04 17:59:37 +0900

    init2

---
+++ adapter/out/in-memory/src/test/java/com/pigeon3/ssamantle/adapter/out/inmemory/InMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigeon3.ssamantle.adapter.out.inmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ adapter/out/rdb/src/test/java/com/pigeon3/ssamantle/adapter/out/mysql/MysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigeon3.ssamantle.adapter.out.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
