52f5bae17a063cd2e07daa2ce3694d4864494147
SeungHyeok Yun <162292720+SeungHyeokYoon@users.noreply.github.com>
2025-12-21 18:25:04 +0900

    feat: add daily scheduler

--- bootstrap/src/main/java/com/pigeon3/ssamantle/bootstrap/BootstrapApplication.java
+++ bootstrap/src/main/java/com/pigeon3/ssamantle/bootstrap/BootstrapApplication.java
@@ -3,15 +3,18 @@ package com.pigeon3.ssamantle.bootstrap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan(basePackages = {
-	"com.pigeon3.ssamantle.bootstrap",
-	"com.pigeon3.ssamantle.adapter.in.http",
-	"com.pigeon3.ssamantle.adapter.out.rdb",
-	"com.pigeon3.ssamantle.adapter.out.inmemory",
-	"com.pigeon3.ssamantle.adapter.out.inference",
-	"com.pigeon3.ssamantle.application"
+        "com.pigeon3.ssamantle.adapter.in.http",
+        "com.pigeon3.ssamantle.adapter.in.scheduler",
+        "com.pigeon3.ssamantle.adapter.out.inference",
+        "com.pigeon3.ssamantle.adapter.out.inmemory",
+        "com.pigeon3.ssamantle.adapter.out.rdb",
+        "com.pigeon3.ssamantle.application",
+        "com.pigeon3.ssamantle.bootstrap"
 })
 public class BootstrapApplication {
 
